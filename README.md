e39a9691b9b715be7e49028db9d0496cd3adcf04
Naseer Ahmed <112649474+mr-luckey@users.noreply.github.com>
2025-06-13 03:34:10 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# jobseeker_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/jobseeker_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.jobseeker_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
